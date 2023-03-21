216c2bed01c16b224e6693fd1e1a4247a54dc787
dg1223 <alavi1223@hotmail.com>
2023-03-21 18:02:08 -0400

    jsx dynamic data loading

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import "./App.css";
 const number = 5555;
 const singer1 = { name: "Dr. Mahfuz", job: "singer" };
 const singer2 = { name: "Eva Rahman", job: "singer" };
+const singers = [
+  { name: "Dr. Mahfuz", job: "singer" },
+  { name: "Eva Rahman", job: "singer" },
+  { name: "Agun", job: "Shopno" },
+  { name: "Shuvro", job: "pathor" },
+];
 
 const singerStyle = {
   color: "purple",
@@ -11,11 +17,29 @@ const singerStyle = {
 };
 
 function App() {
+  const nayoks = [
+    "Rubel",
+    "Bapparaj",
+    "Kuber",
+    "Jashim",
+    "Salman Shah",
+    "Riyaz",
+    "Anwar",
+  ];
   return (
     <div className="App">
-      <Person name="Rubel" nayika="moushumi"></Person>
-      <Person name="BappaRaj" nayika="cheka"></Person>
-      <Person name="Kuber" nayika="Kopila"></Person>
+      {nayoks.map((nayok) => (
+        <li>Name: {nayok}</li>
+      ))}
+      {/* {nayoks.map((nayok) => (
+        <Person name={nayok}></Person>
+      ))} */}
+      {singers.map((singer) => (
+        <Person name={singer.name}></Person>
+      ))}
+      {/* <Person name={nayoks[0]} nayika="moushumi"></Person>
+      <Person name={nayoks[1]} nayika="cheka"></Person>
+      <Person name={nayoks[2]} nayika="Kopila"></Person> */}
       <h5>New component</h5>
       <Friend movie="singham" phone="017777"></Friend>
       <Friend phone="019999"></Friend>
@@ -29,13 +53,13 @@ function Person(props) {
     <div className="person">
       {/* <h1>Sakib Al Hasan</h1> */}
       <h1>{props.name}</h1>
-      <p>Profession: Cricketer</p>
+      <p>{props.nayika}</p>
     </div>
   );
 }
 
 function Friend(props) {
-  console.log(props);
+  // console.log(props);
   return (
     <div className="container">
       <h3>Name: Ajay Devgun</h3>
