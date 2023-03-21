c3867960dfd774b9a4cc1cac11cebd7d986ef2fa
dg1223 <alavi1223@hotmail.com>
2023-03-21 17:26:53 -0400

    jsx components

--- src/App.js
+++ src/App.js
@@ -13,33 +13,31 @@ const singerStyle = {
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>JSX</h1>
-        <div className="container">
-          <h3>Hello React, how are you?</h3>
-        </div>
-        <div className="music">
-          <p>Name: {2222 + number}</p>
-          <p style={singerStyle}>
-            Name: {singer1.name} {singer1.job}
-          </p>
-          <p style={{ color: "blue", backgroundColor: "yellow" }}>
-            Name: {singer2.name} {singer2.job}
-          </p>
-        </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <h5>New component</h5>
+      <Friend></Friend>
+      <Friend></Friend>
+    </div>
+  );
+}
+
+function Person() {
+  return (
+    <div className="person">
+      <h1>Sakib Al Hasan</h1>
+      <p>Profession: Cricketer</p>
+    </div>
+  );
+}
+
+function Friend() {
+  return (
+    <div className="container">
+      <h3>Name: Ajay Devgun</h3>
+      <p>job: maramari</p>
     </div>
   );
 }
